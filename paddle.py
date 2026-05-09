57d7ef8a9c1d450671f5113a72916c22a0573ad2
Richard Olpindo <djricky088@yahoo.com>
2026-05-08 21:37:02 -1000

    Refactored code to make the Paddle class inherit the turtle class

--- paddle.py
+++ paddle.py
@@ -4,21 +4,21 @@ WIDTH = 1
 HEIGHT = 5
 INCREMENT = 20
 
-class Paddle:
+class Paddle(Turtle):
     def __init__(self, position):
-        self.paddle = Turtle()
-        self.paddle.shape('square')
-        self.paddle.color('white')
-        self.paddle.penup()
-        self.paddle.shapesize(stretch_wid= HEIGHT, stretch_len= WIDTH)
-        self.paddle.goto(position[0],position[1])
+        super().__init__()
+        self.shape('square')
+        self.color('white')
+        self.penup()
+        self.shapesize(stretch_wid= HEIGHT, stretch_len= WIDTH)
+        self.goto(position[0],position[1])
 
     def move_up(self):
-        if self.paddle.ycor() < 250:
-            new_y = self.paddle.ycor() + INCREMENT
-            self.paddle.goto(self.paddle.xcor(), new_y)
+        if self.ycor() < 250:
+            new_y = self.ycor() + INCREMENT
+            self.goto(self.xcor(), new_y)
 
     def move_down(self):
-        if self.paddle.ycor() > -250:
-            new_y = self.paddle.ycor() - INCREMENT
-            self.paddle.goto(self.paddle.xcor(), new_y)
+        if self.ycor() > -250:
+            new_y = self.ycor() - INCREMENT
+            self.goto(self.xcor(), new_y)
