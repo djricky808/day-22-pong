a34ae09bda638acdb71b36ce3804a30e3925ba17
Richard Olpindo <djricky088@yahoo.com>
2026-05-09 21:07:05 -1000

    Added conditions for when the ball got to a certain point in the Y-Axis (bouncing off the wall)

--- ball.py
+++ ball.py
@@ -2,16 +2,25 @@ from turtle import Turtle
 
 INCREMENT = 10
 
+
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
+        self.wall_direction = "up"
         self.shape("circle")
         self.color("white")
         self.penup()
         self.goto(0,0)
 
     def move_ball(self):
-        if self.xcor() < 380 and self.ycor() < 280:
+        if self.ycor() >= 280:
+            self.wall_direction = "down"
+        elif self.ycor() <= -280:
+            self.wall_direction = "up"
+        if self.wall_direction == "up" :
             new_x = self.xcor() + INCREMENT
             new_y = self.ycor() + INCREMENT
-            self.goto(new_x,new_y)
+        else:
+            new_x = self.xcor() + INCREMENT
+            new_y = self.ycor() - INCREMENT
+        self.goto(new_x,new_y)
