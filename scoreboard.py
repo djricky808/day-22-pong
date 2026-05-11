47a2a97703647bbe96ec73f2ee77b089eaf2a59d
Richard Olpindo <djricky088@yahoo.com>
2026-05-10 16:59:43 -1000

    Increased font side and adjusted score positioning

--- scoreboard.py
+++ scoreboard.py
@@ -1,14 +1,14 @@
 from turtle import Turtle
 
 ALIGNMENT = "center"
-FONT = ('Courier', 30, "bold")
+FONT = ('Courier', 80, "bold")
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.color('white')
         self.penup()
-        self.goto(x=0, y=250)
+        self.goto(x=0, y=220)
         self.left_score = 0
         self.right_score = 0
         self.hideturtle()
