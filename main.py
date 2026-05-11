a70eb894f58f0fddecbac16ef59accacb9f6b37b
Richard Olpindo <djricky088@yahoo.com>
2026-05-10 16:13:25 -1000

    Added ball bouncing logic with the paddle

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ while game_is_on:
     ball.move_ball()
 
     if ball.ycor() >= 280 or ball.ycor() <= -280 :
-        ball.bounce()
+        ball.bounce_y()
+
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
